3f8cbc976762eee5e2915b1e9bf0ae04daa2fbb1
Dum6o <edwardasas@gmail.com>
2023-04-15 16:06:59 +0300

    #4 Update stories fetch

--- src/app/blocks/about-block/about-block.component.spec.ts
+++ src/app/blocks/about-block/about-block.component.spec.ts
@@ -5,11 +5,18 @@ import { FirestoreService } from 'src/app/services/firestore.service';
 import { Mock } from 'src/app/testing/mock-data.test';
 import { MockModule } from 'src/app/testing/mock-module.test';
 import { AboutBlockComponent } from './about-block.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SessionStorageService } from 'src/app/services/session-storage.service';
+import { of } from 'rxjs';
+import { Collection } from 'src/app/constants/collections';
+import { StorageKey } from 'src/app/constants/storage-keys';
 
 describe('AboutBlockComponent', () => {
   let component: AboutBlockComponent;
   let fixture: ComponentFixture<AboutBlockComponent>;
   let deviceDetector: DeviceDetectorService;
+  let firestoreService: FirestoreService;
+  let firestoreSpy: jest.SpyInstance;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,9 +25,11 @@ describe('AboutBlockComponent', () => {
         TakeEveryPipe
       ],
       imports: [
+        BrowserAnimationsModule,
         MockModule
       ],
       providers: [
+        SessionStorageService,
         { provide: FirestoreService, useValue: MockModule.firestoreService },
         { provide: DeviceDetectorService, useValue: MockModule.deviceDetector }
       ]
@@ -31,13 +40,21 @@ describe('AboutBlockComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AboutBlockComponent);
     component = fixture.componentInstance;
+
     deviceDetector = TestBed.inject(DeviceDetectorService);
+    firestoreService = TestBed.inject(FirestoreService);
+    
+    firestoreSpy = jest.spyOn(firestoreService, 'getCollectionItems')
+                    .mockReturnValue(of(Mock.stories));
+
+    const sessionStorage = new SessionStorageService();
+    sessionStorage.remove(StorageKey.STORIES);
   });
 
   test('should create', () => {
     expect(component).toBeTruthy();
     expect(component.isMobile).toBeFalsy();
-    expect(component.dataList.length).toBe(0);
+    expect(component.dataList).toBeNull();
   });
 
   test('should detect is mobile on init', () => {
@@ -46,5 +63,17 @@ describe('AboutBlockComponent', () => {
     component.ngOnInit();
     expect(detectorSpy).toBeCalledTimes(1);
     expect(component.isMobile).toBeTruthy();
-  })
+  });
+
+  test('should init about tab data', () => {
+    component.ngOnInit();
+    expect(firestoreSpy).toHaveBeenLastCalledWith(Collection.STORIES);
+    expect(component.dataList.length).toBe(2);
+  });
+
+  test('should disable loading after init', () => {
+    expect(component.loading).toBeTruthy();
+    component.ngOnInit();
+    expect(component.loading).toBeFalsy();
+  });
 });
--- src/app/blocks/about-block/about-block.component.ts
+++ src/app/blocks/about-block/about-block.component.ts
@@ -1,30 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceDetectorService } from 'src/app/services/device-detector.service';
-import { AboutDescription } from 'src/app/models/data/about.model';
+import { Story } from 'src/app/models/data/story.model';
+import { FirestoreService } from 'src/app/services/firestore.service';
+import { SessionStorageService } from 'src/app/services/session-storage.service';
+import { Unsubscribable } from 'src/app/operators/unsubscribtion.operator';
+import { StorageKey } from 'src/app/constants/storage-keys';
+import { Collection } from 'src/app/constants/collections';
+import { takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-about-block',
   templateUrl: './about-block.component.html',
   styleUrls: ['./about-block.component.scss']
 })
-export class AboutBlockComponent implements OnInit {
+export class AboutBlockComponent extends Unsubscribable() implements OnInit {
 
-  dataList: AboutDescription[] = [];
+  dataList: Story[] = [];
   isMobile = false;
+  loading = true;
 
-  constructor(private deviceDetector: DeviceDetectorService) { }
+  constructor(private deviceDetector: DeviceDetectorService,
+              private firestore: FirestoreService,
+              private sessionStorage: SessionStorageService) {
+    super();
+    this.dataList = this.sessionStorage.get(StorageKey.STORIES) as Story[] | [];
+  }
 
   ngOnInit(): void {
     this.isMobile = this.deviceDetector.isMobile();
-    this.dataList = [
-      { title: 'Leisure', content: 'During free time I do lot of stuff. Sussex result matter any end see. It speedily me addition weddings vicinity in pleasure. Happiness commanded an conveying breakfast in.' },
-      { title: 'Some First', content: 'Meant balls it if up doubt small purse. Required his you put the outlived answered position. An pleasure exertion if believed provided to. All led out world these music while asked. Paid mind even sons does he door no. Attended overcame repeated it is perceive marianne in. In am think on style child of. Servants moreover in sensible he it ye possible.' },
-      { title: 'Second', content: 'Though all excuse ladies rather regard assure yet. If feelings so prospect no as raptures quitting.' },
-      { title: 'Third maybe', content: 'You disposal strongly quitting his endeavor two settling him. Manners ham him hearted hundred expense. Get open game him what hour more part. Adapted as smiling of females oh me journey exposed concern.' },
-      { title: 'Fourth', content: 'Adieus except say barton put feebly favour him. Entreaties unpleasant sufficient few pianoforte discovered uncommonly ask. Morning cousins amongst in mr weather do neither. Warmth object matter course active law spring six. Pursuit showing tedious unknown winding see had man add.' },
-      { title: 'Simple fifth element', content: 'Next his only boy meet the fat rose when. Do repair at we misery wanted remove remain income. Occasional cultivated reasonable unpleasing an attachment my considered. Having ask and coming object seemed put did admire figure. Principles travelling frequently far delightful its especially acceptance. ' },
-    ];
-
+    if (!this.dataList || this.dataList.length === 0) {
+      this.firestore.getCollectionItems<Story>(Collection.STORIES)
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(data => {
+        this.dataList = data;
+        this.sessionStorage.save(StorageKey.STORIES, this.dataList);
+        this.loading = false;
+      });
+    } else {
+      this.loading = false;
+    }
   }
 
 }
